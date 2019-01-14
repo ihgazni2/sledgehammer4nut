7d40366c24032ec98cf72d98cd9d22f6c082c76e
dli <ihgazni2010@hotmail.com>
2019-01-14 01:44:15 +0000

    classified funcs

---
+++ build/lib/sldghmmr4nut/txt.py
@@ -0,0 +1,245 @@
+import elist.elist as elel
+import estring.estring as eses
+import numpy as np
+import sldghmmr4nut.ndarr.do as ndo
+
+
+def wrap(s,fn,*args,**kwargs):
+    ndarr = ndo.convert.txt2ndarr(s,**kwargs)
+    ndarr = fn(ndarr,*args)
+    s = ndo.convert.ndarr2txt(ndarr)
+    return(s)
+
+def txtfilter(s,actions,**kwargs):
+    actions = ndo.creat_action_list(actions,**kwargs)
+    ndarr = ndo.convert.txt2ndarr(s,**kwargs)
+    ndarr = ndo.filter(ndarr,actions)
+    s = ndo.convert.ndarr2txt(ndarr)
+    return(s)
+
+def swap_dimension(s,**kwargs):
+    s = wrap(s,ndo.swap_dimension,[],**kwargs)
+    return(s)
+
+def fliplr(s,**kwargs):
+    s = wrap(s,np.fliplr,[],**kwargs)
+    return(s)
+
+def flipud(s,**kwargs):
+    s = wrap(s,np.flipud,[],**kwargs)
+    return(s)
+
+def to_ancient_chinese(s,**kwargs):
+    s = txtfilter(s,[ndo.swap_dimension,np.fliplr],**kwargs)
+    return(s)
+
+def from_ancient_chinese(s,**kwargs_rows):
+    s = txtfilter(s,[np.fliplr,ndo.swap_dimension],**kwargs)
+    return(s)
+
+def swap_row(s,rowseq1,rowseq2,**kwargs):
+    s = wrap(s,ndo.swap_row,[rowseq1,rowseq2],**kwargs)
+    return(s)
+
+def swap_rows(s,rowseqs1,rowseqs2,**kwargs):
+    s = wrap(s,ndo.swap_rows,[rowseqs1,rowseqs2],**kwargs)
+    return(s)
+
+def swap_col(s,colseq1,colseq2,**kwargs):
+    s = wrap(s,ndo.swap_col,[colseq1,colseq2],**kwargs)
+    return(s)
+
+def swap_cols(s,colseqs1,colseqs2,**kwargs):
+    s = wrap(s,ndo.swap_cols,[colseqs1,colseqs2],**kwargs)
+    return(s)
+
+def insert_row(s,rowseq,row,**kwargs):
+    row = eses.str2chnums(row)
+    s = wrap(s,ndo.insert_row,[rowseq,row],**kwargs)
+    return(s)
+
+def insert_rows(s,rowseq,rows,**kwargs):
+    rows = elel.mapv(rows,eses.str2chnums)
+    s = wrap(s,ndo.insert_rows,[rowseq,rows],**kwargs)
+    return(s)
+
+def insert_col(s,colseq,col,**kwargs):
+    col = eses.str2chnums(col)
+    s = wrap(s,ndo.insert_col,[colseq,col],**kwargs)
+    return(s)
+
+def insert_cols(s,colseq,cols,**kwargs):
+    cols = elel.mapv(cols,eses.str2chnums)
+    s = wrap(s,ndo.insert_cols,[colseq,cols],**kwargs)
+    return(s)
+
+def append_col(s,col):
+    col = eses.str2chnums(col)
+    s = wrap(s,ndo.append_col,[col],**kwargs)
+    return(s)
+
+def append_cols(s,cols):
+    cols = elel.mapv(cols,eses.str2chnums)
+    s = wrap(s,ndo.append_cols,[cols],**kwargs)
+    return(s)
+
+def append_row(s,row):
+    row = eses.str2chnums(row)
+    s = wrap(s,ndo.append_row,[row],**kwargs)
+    return(s)
+
+def append_rows(s,rows):
+    rows = elel.mapv(rows,eses.str2chnums)
+    s = wrap(s,ndo.append_rows,[row],**kwargs)
+    return(s)
+
+def prepend_col(s,col):
+    col = eses.str2chnums(col)
+    s = wrap(s,ndo.prepend_col,[col],**kwargs)
+    return(s)
+
+def prepend_cols(s,cols):
+    cols = elel.mapv(cols,eses.str2chnums)
+    s = wrap(s,ndo.prepend_cols,[cols],**kwargs)
+    return(s)
+
+def prepend_row(s,row):
+    row = eses.str2chnums(row)
+    s = wrap(s,ndo.prepend_row,[row],**kwargs)
+    return(s)
+
+def prepend_rows(s,rows):
+    rows = elel.mapv(rows,eses.str2chnums)
+    s = wrap(s,ndo.prepend_rows,[row],**kwargs)
+    return(s)
+
+def crop(s,top,left,bot,right,**kwargs):
+    s = wrap(s,ndo.crop,[top,left,bot,right],**kwargs)
+    return(s)
+
+def slct(s,rowseqs,colseqs,**kwargs):
+    s = wrap(s,ndo.slct,[rowseqs,colseqs],**kwargs)
+    return(s)
+
+def slct_col(s,colseq,**kwargs):
+    s = wrap(s,ndo.slct_col,[colseq],**kwargs)
+    return(s)
+
+def slct_cols(s,colseqs,**kwargs):
+    s = wrap(s,ndo.slct_cols,[colseqs],**kwargs)
+    return(s)
+
+def slct_row(s,rowseq,**kwargs):
+    s = wrap(s,ndo.slct_row,[rowseq],**kwargs)
+    return(s)
+
+def slct_rows(s,rowseqs,**kwargs):
+    s = wrap(s,ndo.slct_rows,[rowseqs],**kwargs)
+    return(s)
+
+def rows(s,**wargs):
+    s = wrap(s,ndo.rows,[],**kwargs)
+    return(s)
+
+def cols(s,**kwargs):
+    s = wrap(s,ndo.cols,[],**kwargs)
+    return(s)
+
+def rplc_col(s,colseq,col,**kwargs):
+    col = eses.str2chnums(col)
+    s = wrap(s,ndo.rplc_col,[colseq,col],**kwargs)
+    return(s)
+
+def rplc_cols(s,colseqs,cols,**kwargs):
+    cols = elel.mapv(cols,eses.str2chnums)
+    cols = np.array(cols)
+    s = wrap(s,ndo.rplc_cols,[colseqs,cols],**kwargs)
+    return(s)
+
+def rplc_row(s,rowseq,row,**kwargs):
+    row = eses.str2chnums(row)
+    s = wrap(s,ndo.rplc_row,[rowseq,row],**kwargs)
+    return(s)
+
+def rplc_rows(s,rowseqs,rows,**kwargs):
+    rows = elel.mapv(rows,eses.str2chnums)
+    rows = np.array(rows)
+    s = wrap(s,ndo.rplc_rows,[rowseqs,rows],**kwargs)
+    return(s)
+
+def rplc_blk(s,top,left,bot,right,blk,**kwargs):
+    s = wrap(s,ndo.rplc_blk,[top,left,bot,right,blk],**kwargs)
+    return(s)
+
+def rm_col(s,colseq,**kwargs):
+    s = wrap(s,ndo.rm_col,[colseq],**kwargs)
+    return(s)
+
+def rm_cols(s,colseqs,**kwargs):
+    s = wrap(s,ndo.rm_cols,[colseqs],**kwargs)
+    return(s)
+
+def rm_row(s,rowseq,**kwargs):
+    s = wrap(s,ndo.rm_row,[rowseq],**kwargs)
+    return(s)
+
+def rm_rows(s,rowseqs,**kwargs):
+    s = wrap(s,ndo.rm_rows,[rowseqs],**kwargs)
+    return(s)
+
+def ccwrot90(s,**kwargs):
+    s = wrap(s,ndo.ccwrot90,[],**kwargs)
+    return(s)
+
+def ccwrot180(s,**kwargs):
+    s = wrap(s,ndo.ccwrot180,[],**kwargs)
+    return(s)
+
+def ccwrot270(s,**kwargs):
+    s = wrap(s,ndo.ccwrot270,[],**kwargs)
+    return(s)
+
+def cwrot90(s,**kwargs):
+    s = wrap(s,ndo.ccwrot90,[],**kwargs)
+    return(s)
+
+def cwrot180(s,**kwargs):
+    s = wrap(s,ndo.ccwrot180,[],**kwargs)
+    return(s)
+
+def cwrot270(s,**kwargs):
+    s = wrap(s,ndo.ccwrot270,[],**kwargs)
+    return(s)
+
+def rowtop_colleft(s,**kwargs):
+    s = wrap(s,ndo.rowtop_colleft,[],**kwargs)
+    return(s)
+
+def rowtop_colright(s,**kwargs):
+    s = wrap(s,ndo.rowtop_colright,[],**kwargs)
+    return(s)
+
+def rowbot_colright(s,**kwargs):
+    s = wrap(s,ndo.rowbot_colright,[],**kwargs)
+    return(s)
+
+def rowbot_colleft(s,**kwargs):
+    s = wrap(s,ndo.rowbot_colleft,[],**kwargs)
+    return(s)
+
+def rowleft_coltop(s,**kwargs):
+    s = wrap(s,ndo.rowleft_coltop,[],**kwargs)
+    return(s)
+
+def rowright_coltop(s,**kwargs):
+    s = wrap(s,ndo.rowright_coltop,[],**kwargs)
+    return(s)
+
+def rowright_colbot(s,**kwargs):
+    s = wrap(s,ndo.rowright_colbot,[],**kwargs)
+    return(s)
+
+def rowleft_colbot(s,**kwargs):
+    s = wrap(s,ndo.rowleft_colbot,[],**kwargs)
+    return(s)
+
---
+++ sldghmmr4nut/ndarr/ndarr.spec
@@ -0,0 +1,36 @@
+#exif 
+# > xexif.man("Orientation")
+# { '1': 'top-left',
+  # '2': 'top-right',
+  # '3': 'bot-right',
+  # '4': 'bot-left',
+  # '5': 'left-top',
+  # '6': 'right-top',
+  # '7': 'right-bot',
+  # '8': 'left-bot' }
+# --------------------------------
+
+# { '1': 'normal (raw same as visual)',
+  # '2': 'reverse-horizonally(from raw to visual)',
+  # '3': 'rotate-180(from raw to visual)',
+  # '4': 'reverse-vertically(from raw to visual)',
+  # '5': 'rotate-clockwise-90 and reverse-horizonally(from raw to visual)',
+  # '6': 'rotate-clockwise-90(from raw to visual)',
+  # '7': 'rotate-counter-clockwise-90 and reverse-horizonally(from raw to visual)',
+  # '8': 'rotate-counter-clockwise-90 (from raw to visual)' }
+# --------------------------------
+
+# { '1': 'The 0th row is at the visual top of the image, and the 0th column is the visual left-hand side',
+  # '2': 'The 0th row is at the visual top of the image, and the  0th column is the visual right-hand side',
+  # '3': 'The 0th row is at the visual bottom of the image, and the 0th column is the visual right-hand side',
+  # '4': 'The 0th row is at the visual bottom of the image, and the 0th column is the visual left-hand side',
+  # '5': 'The 0th row is the visual left-hand side of the image, and the 0th column is the visual top',
+  # '6': 'The 0th row is the visual right-hand side of the image, and the 0th column is the visual top',
+  # '7': 'The 0th row is the visual right-hand side of the image, and the 0th column is the visual bottom',
+  # '8': 'The 0th row is the visual left-hand side of the image, and the 0th column is the visual bottom' }
+# --------------------------------
+
+# DC-008-Translation-2016-E: Page 30-35
+# --------------------------------
+
+
